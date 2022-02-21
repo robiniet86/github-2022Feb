093e94e6304f18a48f3997aa145d209f9fdbca6d
Robin Singh <robin.iet86@gmail.com>
2022-02-21 11:13:29 +0530

    Test File to test Tutorials Point Code.

---
+++ GOG/IO/TutorialPoint.py
@@ -0,0 +1,23 @@
+print("Hello \
+world")
+
+a = 2
+b = 2
+c = a \
++ b
+
+q =w =e =4
+q,w,e = 1,2,3
+print("input : " + str(q)+ " " + str(w))
+
+
+l1 = [1,2,3,4]
+print(l1)
+
+#Python Dictionary
+a = {2:2,3:4}
+print(a[2])
+
+print('###############')
+for i in range(1,4,2):
+    print(i)
